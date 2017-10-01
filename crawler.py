4a138ad4867aee49e3b245ac73e603a9bb6e4135
Vinh Ngu <vinh-ngu@hotmail.com>
2017-10-01 05:59:06 +0200

    Refactoring.

--- crawler.py
+++ crawler.py
@@ -1,38 +1,54 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 
 import mechanize
 import datetime
 from lxml import html, cssselect
 
-now = datetime.datetime.now()
-br = mechanize.Browser()
-
 
 def select_form_by_name(name):
-	for form in br.forms():
-		if form.name == name:
-			return form
-
-response = br.open('http://www.hvv.de/fahrplaene/abfahrten/index.php')
-
-form  = br.select_form(name="departure-listing-form")
-
-br.form["station"] = "Saarlandstrasse"
-br.form["on"] = str(now.day) + "." + str(now.month) + "." + str(now.year)
-br.form["at"] =  str(now.hour) + ":" + str(now.minute)
+    for form in br.forms():
+        if form.name == name:
+            return form
 
 
+def request_station_info(station):
+    br = mechanize.Browser()
+    br.open('http://www.hvv.de/fahrplaene/abfahrten/index.php')
+    br.select_form(name="departure-listing-form")
 
-tree = html.fromstring(br.submit().read())
-elements  = tree.cssselect(".standard.departures-listing td")
-stationen =  elements[::2]
-print elements
-for station in stationen:
-	if isinstance(station.text, unicode):
-		print station.text.encode("utf-8"), station.text
-	print station.text
+    # Make form request.
+    now = datetime.datetime.now()
+    br.form["station"] = station
+    br.form["on"] = str(now.day) + "." + str(now.month) + "." + str(now.year)
+    br.form["at"] = str(now.hour) + ":" + str(now.minute)
+    return br.submit().read()
 
 
+def save_string_to_file(filename, string):
+    with open(filename, 'w') as file:
+        file.write(string)
 
+
+def get_string_of_file(filename):
+    with open(filename, 'r') as file:
+        return file.read()
+
+
+if __name__ == "__main__":
+    file_name = "response_html.html"
+    response_html_str = request_station_info("Hallerstrasse")
+    # response_html_str = get_string_of_file(file_name)
+
+    tree = html.fromstring(response_html_str)
+    stations_unfiltered = tree.cssselect(".standard.departures-listing td")
+    station_filtered = []
+    for station in stations_unfiltered:
+        if not station.text is None:
+            station_filtered.append(station.text.encode("utf-8").strip())
+
+    stations = station_filtered[0::2]
+    etas = station_filtered[1::2]
+    for station, time in zip(stations, etas):
+        print station + "\t" + time
